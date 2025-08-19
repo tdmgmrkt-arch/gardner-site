336dbbd0a441d8f311576bac1a43f861a4400ed6
kennethlerch <kenneth.lerch@gmail.com>
2025-08-19 12:28:22 -0700

    Force case update for Hydro.tsx

--- src/HydroJet.tsx
+++ src/HydroJet.tsx
@@ -1,5 +1,5 @@
 import { Header } from "./components/Header";
-import { Hydro } from "./components/hydro";
+import { Hydro } from "./components/Hydro";
 import { Footer } from "./components/Footer";
 
 export default function HydroJet() {
